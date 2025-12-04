ef63c15f1a174692dbc284bd056ddba80b344912
ROZEALT <arnold7059466@stucsd83.org>
2025-12-04 08:46:45 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-export default defineConfig({
-  base: '/DrawStudio/', 
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: '/DrawStudio/',
   server: {
     host: "::",
     port: 8080,
-  },
-});
